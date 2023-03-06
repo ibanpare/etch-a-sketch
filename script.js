8281b08d7b85c6d9648f6ba458734c68435ef85c
ibanpare <13278805+ibanpare@users.noreply.github.com>
2023-03-06 21:15:56 +0100

    Fix typos and add pseudocode

---
+++ script.js
@@ -0,0 +1,17 @@
+/* Create a webpage with a 16x16 grid of square divs
+
+If mouse hovers over a div,
+the div changes color
+
+If mouse click button at the top of the screen
+a popup asks the number of squares per side of the new grid
+once entered, the existing grid should be removed 
+and a new grid should be generated in the same total space as before (e.g. 960px wide) 
+Set the limit for the user input to a maximum of 100
+
+Extra Credit
+Instead of just changing the color of a square from black to white (for example), 
+have each pass through with the mouse change it to a completely random RGB value. 
+Then try having each pass just add another 10% of black to it so that only after 10 passes is the square completely black.
+
+*/

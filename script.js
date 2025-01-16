78aa94177dfba9320019c6bc85da6d441a927db2
ibanpare <13278805+ibanpare@users.noreply.github.com>
2025-01-16 21:06:28 +0100

    feat: add reset capabilities

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 const container = document.querySelector("#container");
+const changeSizeBtn = document.querySelector("#changeSizeBtn");
+const resetBtn = document.querySelector("#resetBtn");
+const squares = document.querySelectorAll(".square");
 
-for (let i = 1; i <= 256; i++) {
-    const littleSquare = document.createElement("div");
-    littleSquare.classList.add("square");
-    container.appendChild(littleSquare);
+function drawGrid(size) {
+    for (let i = 1; i <= size; i++) {
+        const littleSquare = document.createElement("div");
+        littleSquare.classList.add("square");
+        container.appendChild(littleSquare);
+    }
 }
 
-const squares = document.querySelectorAll(".square");
+function resetGrid() {
+    container.textContent = '';
+}
+
 squares.forEach((square) => {
     square.addEventListener("mouseover", () => {
         square.classList.add("colored");
     })
 })
+
+resetBtn.addEventListener("click", () => {
+    resetGrid();
+})
+
+changeSizeBtn.addEventListener("click", () => {
+    drawGrid(prompt("How many squares do you want? Pick a number from 1 to 100."));
+})
+
+drawGrid(256);
+
